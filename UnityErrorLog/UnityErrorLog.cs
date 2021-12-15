edca6c2a6b938bf8d8ef81ee34fec52cc1b7f8a0
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2021-12-15 14:05:38 +0000

    Update UnityErrorLog.cs

--- UnityErrorLog/UnityErrorLog.cs
+++ UnityErrorLog/UnityErrorLog.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Modding;
+using UnityEngine;
 
 namespace UnityErrorLog
 {
@@ -18,12 +19,25 @@ namespace UnityErrorLog
 		{
 			return "0.1";
 		}
-        
+
+        private static readonly SimpleLogger UnityLogger = new SimpleLogger("UNITY");
+
         public override void Initialize()
         {
+            if (ModHooks.GetMod("QoL") is Mod) return;
+
             Log("Initializing Mod...");
-            
-            
+
+            Application.SetStackTraceLogType(LogType.Exception, StackTraceLogType.Full);
+            Application.logMessageReceived += HandleLog;
+        }
+
+        private static void HandleLog(string condition, string stacktrace, LogType type)
+        {
+            if (type != LogType.Exception) return;
+
+            UnityLogger.LogError(condition);
+            UnityLogger.LogError(stacktrace);
         }
     }
 }
